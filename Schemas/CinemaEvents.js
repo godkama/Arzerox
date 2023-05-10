e516e2ba4524aa62eeb1f655ba4efc7500c18ead
LordKamaYT <steamnitz@gmail.com>
2023-05-10 18:41:54 +0200

    Automatic commit

--- Schemas/CinemaEvents.js
+++ Schemas/CinemaEvents.js
@@ -1,19 +1,19 @@
 const { model, Schema } = require("mongoose");
 
-module.exports = {
+module.exports = model(
   "eventSchema",
   new Schema({
-  eventType: String,
-  economyType: String,
-  PPID: String,
-  PPSecret: String,
-  return_url: String,
-  cancel_url: String,
-  GuildID: String,
-  seatDisposition: String,
-  eventCreator: String,
-  eventDateTime: Date,
-  channelType: String,
-  customVideo: String,
-});
-}
+    eventType: String,
+    economyType: String,
+    PPID: String,
+    PPSecret: String,
+    return_url: String,
+    cancel_url: String,
+    GuildID: String,
+    seatDisposition: String,
+    eventCreator: String,
+    eventDateTime: Date,
+    channelType: String,
+    customVideo: String,
+  })
+);
