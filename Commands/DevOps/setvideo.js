0c9e1819b31a472e2be9e321379f355b20df39cb
LordKamaYT <steamnitz@gmail.com>
2023-05-20 10:38:18 +0200

    Automatic commit

---
+++ Commands/DevOps/setvideo.js
@@ -0,0 +1,46 @@
+const User = require("../../Models/User");
+const {
+  Client,
+  Message,
+  EmbedBuilder,
+  AttachmentBuilder,
+} = require("discord.js");
+
+module.exports = {
+  name: "commandname",
+  description: `command description`,
+  developer: false,
+  premium: false,
+  owneronly: false,
+  bloxia: false,
+  /**
+   *
+   * @param {Message} message
+   * @param {*} args
+   * @param {Client} client
+   */
+  async execute(message, args, commandName, client, Discord) {
+    //Code
+  },
+};
+
+const attachment = message.attachments.first();
+
+if (attachment && attachment.contentType === "video/mp4") {
+  const voiceChannel = message.member.voice.channel;
+
+  if (!voiceChannel) {
+    return message.reply("You need to be in a voice channel to set the video.");
+  }
+
+  try {
+    const connection = await voiceChannel.join();
+    connection.play(attachment.url);
+    message.reply("Video set successfully!");
+  } catch (error) {
+    console.error(error);
+    message.reply("Error occurred while setting the video.");
+  }
+} else {
+  message.reply("Please attach an MP4 file to set as the video.");
+}
