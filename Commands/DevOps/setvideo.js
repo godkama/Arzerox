fba07cd1b79b3a768b3b3e8cfb105fd2e08faf53
LordKamaYT <steamnitz@gmail.com>
2023-05-20 10:39:35 +0200

    Automatic commit

--- Commands/DevOps/setvideo.js
+++ Commands/DevOps/setvideo.js
@@ -20,27 +20,27 @@ module.exports = {
    * @param {Client} client
    */
   async execute(message, args, commandName, client, Discord) {
-    //Code
+    const attachment = message.attachments.first();
+
+    if (attachment && attachment.contentType === "video/mp4") {
+      const voiceChannel = message.member.voice.channel;
+
+      if (!voiceChannel) {
+        return message.reply(
+          "You need to be in a voice channel to set the video."
+        );
+      }
+
+      try {
+        const connection = await voiceChannel.join();
+        connection.play(attachment.url);
+        message.reply("Video set successfully!");
+      } catch (error) {
+        console.error(error);
+        message.reply("Error occurred while setting the video.");
+      }
+    } else {
+      message.reply("Please attach an MP4 file to set as the video.");
+    }
   },
 };
-
-const attachment = message.attachments.first();
-
-if (attachment && attachment.contentType === "video/mp4") {
-  const voiceChannel = message.member.voice.channel;
-
-  if (!voiceChannel) {
-    return message.reply("You need to be in a voice channel to set the video.");
-  }
-
-  try {
-    const connection = await voiceChannel.join();
-    connection.play(attachment.url);
-    message.reply("Video set successfully!");
-  } catch (error) {
-    console.error(error);
-    message.reply("Error occurred while setting the video.");
-  }
-} else {
-  message.reply("Please attach an MP4 file to set as the video.");
-}
